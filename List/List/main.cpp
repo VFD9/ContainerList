e4f677d2ce1d608b48f4f1de5448fd31762488b9
JeongGon <niceman1994@naver.com>
2022-06-12 16:21:49 +0900

    2022.06.12 ContainerList v0.1

--- List/List/main.cpp
+++ List/List/main.cpp
@@ -10,7 +10,7 @@ struct List
 };
 
 void AddObject(List* _Next, const int& _Value);
-void Insert(int _where, List* _Next, const int& _Value);
+void Insert(List* head, int idx, const int& _Value);
 void Output(List* _Next);
 
 int main(void)
@@ -23,6 +23,8 @@ int main(void)
 	{
 		AddObject(NumberList, i * 10 + 10);
 	}
+
+	Insert(NumberList, 3, 15);
 
 	Output(NumberList->Back);
 
@@ -43,14 +45,28 @@ void AddObject(List* _Next, const int& _Value)
 		AddObject(_Next->Back, _Value);
 }
 
-void Insert(int _where, List* _Next, const int& _Value)
+void Insert(List* head, int idx, const int& _Value)
 {
-	
+	List* ptr = head;
+	List* temp = ptr;
+
+	List* node = new List;
+	node->Value = _Value;
+	node->Back = nullptr;
+
+	for (int i = 0; i < idx; i++)
+	{
+		temp = ptr;
+		ptr = ptr->Back;
+	}
+	temp->Back = node;
+	node->Back = ptr;
 }
 
 void Output(List* _Next)
 {
 	cout << _Next->Value << endl;
+	//cout << _Next->Back << endl << endl;
 
 	if (_Next->Back != nullptr)
 		Output(_Next->Back);
