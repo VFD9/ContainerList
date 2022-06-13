dc5b0614092dbfb6686815fbd556e411821d07bf
KimJeongGon <niceman1994@naver.com>
2022-06-13 12:52:30 +0900

    2022.06.13 ContainerList v0.2

--- List/List/main.cpp
+++ List/List/main.cpp
@@ -1,4 +1,4 @@
-// ContainerList v0.1
+// ContainerList v0.2
 #include <iostream>
 
 using namespace std;
@@ -9,8 +9,10 @@ struct List
 	List* Back;
 };
 
-void AddObject(List* _Next, const int& _Value);
-void Insert(List* head, int idx, const int& _Value);
+void push_back(List* _Next, const int& _Value);
+void insert(List* _Next, int _Where, const int& _Value);
+void erase(List* _Next, const int _Where);
+//void Insert(List* head, int idx, const int& _Value);
 void Output(List* _Next);
 
 int main(void)
@@ -20,18 +22,19 @@ int main(void)
 	NumberList->Back = nullptr;
 
 	for (int i = 0; i < 10; ++i)
-	{
-		AddObject(NumberList, i * 10 + 10);
-	}
+		push_back(NumberList, i * 10 + 10);
 
-	Insert(NumberList, 3, 15);
+	insert(NumberList, 2, 15);
+	Output(NumberList->Back);
 
+	erase(NumberList, 2);
+	//Insert(NumberList, 3, 15);
 	Output(NumberList->Back);
 
 	return 0;
 }
 
-void AddObject(List* _Next, const int& _Value)
+void push_back(List* _Next, const int& _Value)
 {
 	if (_Next->Back == nullptr)
 	{
@@ -42,31 +45,66 @@ void AddObject(List* _Next, const int& _Value)
 		_Next->Back = Temp;
 	}
 	else
-		AddObject(_Next->Back, _Value);
+		push_back(_Next->Back, _Value);
 }
 
-void Insert(List* head, int idx, const int& _Value)
+void insert(List* _Next, int _Where, const int& _Value)
 {
-	List* ptr = head;
-	List* temp = ptr;
-
-	List* node = new List;
-	node->Value = _Value;
-	node->Back = nullptr;
-
-	for (int i = 0; i < idx; i++)
+	if (_Where == 1)
 	{
-		temp = ptr;
-		ptr = ptr->Back;
+		List* Temp = new List;
+		Temp->Value = _Value;
+		Temp->Back = _Next->Back;
+
+		_Next->Back = Temp;
 	}
-	temp->Back = node;
-	node->Back = ptr;
+	else
+		insert(_Next->Back, _Where - 1, _Value);
 }
+
+List* Front = nullptr;
+
+void erase(List* _Next, const int _Where)
+{
+	if (_Where == 0)
+	{
+		/*List* Temp = _Next->Back;
+		_Next->Back = _Next->Back->Back;*/
+
+		Front->Back = _Next->Back;
+
+		delete _Next;
+		_Next = nullptr;
+	}
+	else
+	{
+		Front = _Next;
+		erase(_Next->Back, _Where - 1);
+	}
+}
+
+//void Insert(List* head, int idx, const int& _Value)
+//{
+//	List* ptr = head;
+//	List* temp = ptr;
+//
+//	List* node = new List;
+//	node->Value = _Value;
+//	node->Back = nullptr;
+//
+//	for (int i = 0; i < idx; i++)
+//	{
+//		temp = ptr;
+//		ptr = ptr->Back;
+//	}
+//	temp->Back = node;
+//	node->Back = ptr;
+//}
 
 void Output(List* _Next)
 {
 	cout << _Next->Value << endl;
-	//cout << _Next->Back << endl << endl;
+	//cout << &_Next << endl << endl;
 
 	if (_Next->Back != nullptr)
 		Output(_Next->Back);
